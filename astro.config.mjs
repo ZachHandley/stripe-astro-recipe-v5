2307b4be0c773cace71b5c4ce5122299b5207845
Zach Handley <zachhandley@gmail.com>
2025-04-26 12:58:30 -0700

    update to v5, remove stripe api stuff for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  site: 'https://www.my-site.dev',
+  output: "server",
+  site: import.meta.env.DEV
+    ? "http://localhost:4321"
+    : "https://www.my-site.dev",
 
   vite: {
-    ssr: { noExternal: ['@splendidlabs/astro'] },
+    ssr: { noExternal: ["@splendidlabs/astro"] },
   },
-})
+});
